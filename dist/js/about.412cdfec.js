b05a05ace13fb9e9da5b468562249d2013b5a290
LimeFlash <googl@mail.com>
2019-03-24 13:09:49 +0300

    upd

--- dist/js/about.412cdfec.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"about"},[t("h1",[e._v("This is an "+e._s(e.$route.name)+" page")])])},s=[],u={name:"home"},o=u,i=t("2877"),l=Object(i["a"])(o,a,s,!1,null,null,null);n["default"]=l.exports}}]);
-//# sourceMappingURL=about.412cdfec.js.map
